2d76080ec36440c0ce4a695efdf475539d0ed43d
RicardoArros <ricardo.arros@u-payments.com>
2022-05-19 21:58:21 -0400

    added .gitignore

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,19 @@
-(function () {
+"use strict";
 
-  "use strict";
+const forms = document.querySelectorAll(".requires-validation");
 
-  const forms = document.querySelectorAll(".requires-validation");
+Array.from(forms).forEach(function (form) {
+  form.addEventListener(
+    "submit",
+    function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
 
-  Array.from(forms).forEach(function(form) {
+        event.stopPropagation();
+      }
 
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
+      form.classList.add("was-validated");
+    },
+    false
+  );
+});
